a833cb8bb8cf86ab6220f03a64be0905c82a3310
Jean Nunes <jean.nunes@uzzipay.com>
2025-06-18 01:51:47 -0400

    Authenticate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { AuthenticateController } from './controllers/authenticate.controller';
 import { UserController } from './controllers/user.controller';
 import { envSchema } from './env';
 import { PrismaService } from './prisma/prisma.service';
@@ -10,8 +12,9 @@ import { PrismaService } from './prisma/prisma.service';
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
-  controllers: [UserController],
+  controllers: [UserController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
